fbafa10d3f8eb1225cfafd8e71cd1a3bac69d098
Adrian <mailforadrian@me.com>
2021-02-16 15:51:50 +0100

    diffableDataSource is now working

--- CollectionView-Transition/HomeViewController.swift
+++ CollectionView-Transition/HomeViewController.swift
@@ -26,6 +26,51 @@ class HomeViewController: UIViewController {
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         collectionView.backgroundColor = .systemBackground
         view.addSubview(collectionView)
+
+        collectionView.register(
+            VegetableCell.self,
+            forCellWithReuseIdentifier: VegetableCell.reuseIdentifier
+        )
+
+        createDataSource()
+        reloadData()
+    }
+
+    func configure<T: SelfConfiguringCell>(
+        _ cellType: T.Type,
+        with vegetable: Vegetable,
+        for indexPath: IndexPath
+    ) -> T {
+        // let the collectionView dequeue a cell of that type
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: cellType.reuseIdentifier,
+            for: indexPath
+        ) as? T else {
+            fatalError("Unable to dequeue \(cellType). Should never fail.")
+        }
+
+        cell.configure(with: vegetable)
+        return cell
+    }
+
+    // MARK: - Data Source
+    func createDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Vegetable>(
+            collectionView: collectionView,
+            cellProvider: { collectionView, indexPath, vegetable in
+                return self.configure(VegetableCell.self, with: vegetable, for: indexPath)
+            }
+        )
+    }
+
+    func reloadData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Vegetable>()
+        snapshot.appendSections(sections)
+
+        for section in sections {
+            snapshot.appendItems(section.items, toSection: section)
+        }
+        dataSource?.apply(snapshot)
     }
 }
 
